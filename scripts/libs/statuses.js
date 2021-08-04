9e12f3ff32243ec014f475225431395e5c3d6a43
!ereHyddeS <87564635+ereHyddeS@users.noreply.github.com>
2021-08-04 21:45:35 +0800

    Update statuses.js

--- scripts/libs/statuses.js
+++ scripts/libs/statuses.js
@@ -1,7 +1,7 @@
 //boxed status effect
 
 const boxed = extend(StatusEffect, "boxed", {
-   localizedName: "Bpxed",
+   localizedName: "Boxed",
    speedMultiplier: 0,
    reloadMultiplier: 0,
    drag: 0
