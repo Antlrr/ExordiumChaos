f2a92455d8686ade9ea59ccb8847c9c6404ef0b2
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-08-26 12:29:59 +0800

    removed planet

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,2 @@
-Events.on(ClientLoadEvent, () => {
-    const planeth = new Planet("planeth", Planets.sun, 1, 1);
-    planeth.generator = new SerpuloPlanetGenerator();
-    planeth.mesh = new HexMesh(planeth, 5);
-    planeth.orbitRadius = 10;
-    planeth.orbitTime = 1.5 * 60;
-    planeth.rotateTime = 30;
-    planeth.bloom = true;
-    planeth.accessible = true;
-    planeth.startSector = 0;
-    planeth.hasAtmosphere = true;
-    planeth.atmosphereColor = Liquids.cryofluid.color;
-    planeth.atmosphereRadIn = 0.1;
-    planeth.atmosphereRadOut = 0.3;
-    planeth.alwaysUnlocked = true;
-    planeth.localizedName = "Planet h";
-
-    for(var i = 0; i < 5; i++){
-        var h = new SectorPreset("h" + i, planeth, i);
-        h.localizedName = "h" + i;
-        h.alwaysUnlocked = true;
-    };
-}); 
 require("libs/statuses")
 require("libs/refresh")
