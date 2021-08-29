2a2d32c7a7f809910023e7e5326b624f3e327882
!ereHyddeS <seanespiritu615@gmail.com>
2021-08-29 21:46:18 +0800

    eeee is so funny bruh

--- scripts/libs/statuses.js
+++ scripts/libs/statuses.js
@@ -7,7 +7,16 @@ const boxedStatus = extend(StatusEffect, "boxedStatus", {
     dragMultiplier: 4,
 });
 
+//eeee status effect
+const eeeeStatus = extend(StatusEffect, "eeee", {
+    localizedName: "EEEE",
+    damage: 4,
+    permanent: true,
+});
+
+
 //Note to self: ADD COMMA
 module.exports = {
     boxedStatus: boxedStatus,
+    eeee: eeee,
 };
