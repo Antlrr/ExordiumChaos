add8343bf4e9754851e1de15cdeeb14847b35437
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-09-27 17:32:10 +0800

    deleting js boxed status

--- scripts/libs/antstatuses.js
+++ scripts/libs/antstatuses.js
@@ -1,12 +1,4 @@
 //referencing pixelcraft's format
-//boxed status effect
-const boxedStatus = extend(StatusEffect, "boxedStatus", {
-    localizedName: "Boxed",
-    speedMultiplier: 0,
-    disarm: true,
-    dragMultiplier: 4,
-});
-
 //eeee status effect
 const eeeeStatus = extend(StatusEffect, "eeeeStatus", {
     localizedName: "EEEE",
