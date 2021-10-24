50ffb7cea6084ea684635a179fde77e4ad7647ef
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-10-24 14:58:11 +0800

    Update spix.js

--- scripts/units/spix.js
+++ scripts/units/spix.js
@@ -1,7 +1,7 @@
 const spix = extend(UnitType, "spix", {
 	draw(unit) {
 		this.super$draw(unit);
-		Draw.rect("antdustry-spix-spinner", unit.x, unit.y, Time.time * 10);
+		Draw.rect("exordium-spix-spinner", unit.x, unit.y, Time.time * 10);
                 }
              });
 spix.constructor = () => extend(UnitEntity, {})
